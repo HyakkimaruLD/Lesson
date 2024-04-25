83922f08d875458725fb246e04244aa3cf74ff20
Vlad Chernukha <forreadtr@gmail.com>
2024-04-25 21:50:44 +0300

    Create RAM > 10 GB

--- Lesson/Lesson.cpp
+++ Lesson/Lesson.cpp
@@ -20,4 +20,17 @@ int main()
     {
         arr[i].showInfo();
     }
+
+    cout << "=============================\n\n";
+    int amount = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i].RAM > 10)
+        {
+            arr[i].showInfo();
+            amount++;
+        }
+
+        cout << "Amount: " << amount << endl;
+    }
 }
